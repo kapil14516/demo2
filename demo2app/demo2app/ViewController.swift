a53544f1ead5521a858e296d91a7aecfde889cca
kapil14516 <kapil14516@gmail.com>
2024-03-24 15:33:07 +0530

    first initial commit

---
+++ demo2app/demo2app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo2app
+//
+//  Created by Kapil.61086229 on 24/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
